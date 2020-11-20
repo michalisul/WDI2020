1fa0df89d2ef4854efd92e42438205b7bce46366
Michal Sulkiewicz <michals@mac.com>
2020-11-20 16:20:19 +0100

    Initial Commit

---
+++ WDI2020-SmartAquarium/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WDI2020-SmartAquarium
+//
+//  Created by Michal Sulkiewicz on 20/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WDI2020-SmartAquarium/WDI2020_SmartAquariumApp.swift
@@ -0,0 +1,17 @@
+//
+//  WDI2020_SmartAquariumApp.swift
+//  WDI2020-SmartAquarium
+//
+//  Created by Michal Sulkiewicz on 20/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct WDI2020_SmartAquariumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
